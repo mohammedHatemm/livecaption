521ee5fe6a7dad77a96d2537638752d232d60889
mohamed hatem <m.hatem@crocoit.com>
2026-07-31 15:20:21 +0300

    Elsherif : add the roote of the project

---
+++ src/test/java/com/elsherif/livecaption/LivecaptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsherif.livecaption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecaptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
